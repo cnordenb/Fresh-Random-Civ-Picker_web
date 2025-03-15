231504a3cbd0b5fb102805b06febb14e31487877
Carl Nordenbrink <cnordenb@abo.fi>
2025-03-15 14:52:55 +0200

    added remaining log

--- script.js
+++ script.js
@@ -17,6 +17,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
 let civs;
 let iterator = 0;
+let remaining = 45;
 
 function getRandomCiv() {
     if (iterator === 45) {
@@ -27,15 +28,18 @@ function getRandomCiv() {
     let civ = civs.splice(randomIndex, 1)[0];
 
     document.getElementById('output').innerText = `${civ}`;
-    document.getElementById('output2').innerText = `(${iterator + 1}/45)`;
-    const historyLog = document.getElementById('historyLog');
-    historyLog.value = `${civ} (${iterator + 1}/45)\n${historyLog.value}`;
+
+
     iterator++;
+    remaining--;
+    UpdateDrawn(civ);
+    UpdateRemaining();
 }
 
 function reset() {
     const historyLog = document.getElementById('historyLog');
     if (civs && civs.length < 45) historyLog.value = `\n${historyLog.value}`;
+
 
     civs = [
         "Armenians", "Aztecs", "Bengalis", "Berbers", "Bohemians", "Britons", "Bulgarians", "Burgundians",
@@ -46,9 +50,26 @@ function reset() {
         "Tatars", "Teutons", "Turks", "Vietnamese", "Vikings"
     ];
 
+
+
     iterator = 0;
+    remaining = 45;
 
     document.getElementById('output').innerText = "?";
     document.getElementById('output2').innerText = "(0/45)";
+    document.getElementById('output3').innerText = "(45/45)";
+    UpdateRemaining();
 }
 
+function UpdateDrawn(civ) {
+    const historyLog = document.getElementById('historyLog');
+    historyLog.value = `${civ} (${iterator + 1}/45)\n${historyLog.value}`;
+    document.getElementById('output2').innerText = `(${iterator}/45)`;
+
+}
+function UpdateRemaining() {
+    const remainingLog = document.getElementById('remainingLog');
+    const sortedCivs = [...civs].sort();
+    remainingLog.value = sortedCivs.join('\n');
+    document.getElementById('output3').innerText = `(${remaining}/45)`;
+}
